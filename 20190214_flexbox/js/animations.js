4520aabce4e205701c7cb1705470ee7219019dc4
Niek VDV <74327@rocteraa-student.nl>
2019-03-19 12:09:15 +0100

    Voorgaande challenges
    gemaakt voordat ik github repository aanmaakte

---
+++ 20190214_flexbox/js/animations.js
@@ -0,0 +1,99 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.item', {
+    duration: 500,
+    distance: '100px',
+    viewFactor: 0.2,
+    reset: false,
+    mobile: false
+}, 80);
+
+sr.reveal('.nav-item', {
+    duration: 500,
+    distance: '100px',
+    viewFactor: 0.2,
+    origin: 'left',
+    reset: false,
+    mobile: false
+}, 80);
+
+sr.reveal('.content div', {
+    duration: 500,
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.logo', {
+    duration: 1000,
+    distance: '0px',
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 360
+    },
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.logo-text', {
+    duration: 500,
+    scale: '0',
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.sr-verhuur-herotext', {
+    duration: 500,
+    distance: '100px',
+    viewFactor: 0.2,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.sr-verhuur-cards', {
+    duration: 500,
+    scale: '0',
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.sr-verhuur-details-cards', {
+    duration: 500,
+    scale: '0',
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.sr-werkwijze-box', {
+    duration: 500,
+    scale: '0',
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+});
+
+sr.reveal('.sr-catering-photogrid', {
+    duration: 500,
+    distance: '0px',
+    viewFactor: 0.5,
+    reset: false,
+    mobile: false
+}, 100);
+
+sr.reveal('.sr-catering-menukaart', {
+    duration: 500,
+    distance: '100px',
+    viewFactor: 0.01,
+    reset: false,
+    mobile: false
+});
